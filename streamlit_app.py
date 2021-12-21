f7722ed963383bb8c21ad230082685a03aa29590
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 20:10:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="review", layout='centered', initial_sidebar_state
 
 class Section():
   def __init__(self):
-    st.write(type(self).name)
+    st.write(type(self).__name__)
 
 class Graphics(Section):
   def __init__(self):
