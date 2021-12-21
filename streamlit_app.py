31812bac6bcab91d7fb4316bd4026f8a483671bc
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 19:51:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ from typing import Sequence, Tuple
 
 st.set_page_config(page_title="review", layout='centered', initial_sidebar_state="collapsed")
 
-def questionRanked(answers: Sequence[dict]):
+def questionRanked(name: str, answers: Sequence[dict]):
+  st.write(name)
   st.write(str(answers))
+  st.select_slider(name, options=[dict.name for dict in answers])
 
 def main():
   questionRanked([{"name":"a"},
