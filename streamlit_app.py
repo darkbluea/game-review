f6f9e4e41e642ad693b6f624de0527f6dac65e7a
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 19:15:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Neural Network prediction on patient lungs", page_icon="https://www.freeiconspng.com/thumbs/heart-png/heart-png-15.png", layout='centered', initial_sidebar_state="collapsed")
+st.set_page_config(page_title="review", layout='centered', initial_sidebar_state="collapsed")
 
 def main():
   return
