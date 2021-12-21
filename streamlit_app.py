91a6da0f2647670151385b509e705c8fcefff2d8
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 19:21:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title="review", layout='centered', initial_sidebar_state="collapsed")
 
+def questionRanked(answers):
+  st.write(str(answers))
+
 def main():
-  return
+  questionRanked(["a", "b"])
 if __name__ == '__main__':
   main()
