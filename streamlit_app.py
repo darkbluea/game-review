e69b52ed3ad3c74a947f2800d0c4973729266f3a
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 20:05:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,18 @@ from typing import Sequence, Tuple
 st.set_page_config(page_title="review", layout='centered', initial_sidebar_state="collapsed")
 
 
+class Section():
+  def __init__(self)
+    st.write(str(type(self)))
+
+class Graphics(Section):
+  def __init__(self):
+    super(Graphics, self).__init__()
+    st.select_slider("question", options=["a",
+                                          "b"])
+
 def main():
+  st.write("Graphics")
   st.select_slider("question", options=["a",
                                         "b"])
 
