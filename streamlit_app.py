895be17504aeb9dce8420914c16b5e1811949b53
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 19:32:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,11 @@ from typing import Sequence, Tuple
 
 st.set_page_config(page_title="review", layout='centered', initial_sidebar_state="collapsed")
 
-def questionRanked(answers: Sequence[Tuple[str]]):
+def questionRanked(answers: Sequence[dict]):
   st.write(str(answers))
 
 def main():
-  questionRanked(["a", "b"])
+  questionRanked([{name:"a"},
+                  {name:"b"}])
 if __name__ == '__main__':
   main()
