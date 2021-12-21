b5a6df55d496a84056fad0ec3de2337ce3ed5d4b
darkbluea <50586316+darkbluea@users.noreply.github.com>
2021-12-21 19:12:20 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Neural Network prediction on patient lungs", page_icon="https://www.freeiconspng.com/thumbs/heart-png/heart-png-15.png", layout='centered', initial_sidebar_state="collapsed")
+
+def main():
+  return
+if __name__ == '__main__':
+  main()
